21bc9e2391cb21c972f7996a401e265f4375dc3d
root <root@qqc.bitalchemy.net>
2014-07-08 20:31:28 +0000

    fisrt commit

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2014-05-23 19:23:19 +0300"
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Blackdogecoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BlackdogecoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
